1ccb0c78a277a9c6d0b4943de3cc2ee704d45cd7
duinodu <472365351duino@gmail.com>
2016-09-01 17:28:29 +0800

    Try to label motion regions, but failed, since motion regions are not steady.

--- standup.py
+++ standup.py
@@ -17,6 +17,7 @@ d   - stop
 r   - restart
 '''
 
+
 # Python 2/3 compatibility
 from __future__ import print_function
 
@@ -25,6 +26,9 @@ import cv2
 import video
 from common import anorm2, draw_str
 from time import clock
+
+
+_DEBUG=False
 
 # calcOpticalFlowPyrLK
 lk_params = dict( winSize  = (15, 15),
@@ -61,6 +65,74 @@ def draw_hist(im):
     return y
 
 
+color = [(255,0,0),
+         (0,255,0),
+         (0,0,255),
+         (255,255,0),
+         (255,0,255),
+         (0,255,255),
+         (128,0,0),
+         (139,0,0),
+         (165,42,42),
+         (178,34,34),
+         (220,20,60),
+         (255,0,0),
+         (255,99,71),
+         (255,127,80),
+         (205,92,92),
+         (240,128,128),
+         (233,150,122),
+         (250,128,114),
+         (255,160,122),
+         (255,69,0),
+         (255,140,0),
+         (255,165,0),
+         (255,215,0),
+         (240,230,140),
+         (128,128,0),
+         (255,255,0),
+         (154,205,50),
+         (85,107,47),
+         (107,142,35),
+         (124,252,0),
+         (127,255,0),
+         (173,255,47),
+         (0,100,0),
+         (0,128,0),
+         (34,139,34),
+         (0,255,0),
+         (50,205,50),
+         (144,238,144),
+         (152,251,152),
+         (143,188,143),
+         (0,250,154),
+         (0,255,127),
+         (46,139,87),
+         (102,205,170),
+         (60,179,113),
+         (32,178,170),
+         (47,79,79),
+         (0,128,128),
+         (0,139,139),
+         (0,255,255),
+         (0,255,255),
+         (224,255,255),
+         (0,206,209),
+         (64,224,208),
+         (72,209,204),
+         (175,238,238),
+         (127,255,212),
+         (176,224,230),
+         (95,158,160),
+         (70,130,180),
+         (100,149,237),
+         (0,191,255),
+         (30,144,255),
+         (173,216,230),
+         (135,206,235),
+         (135,206,250)]
+
+
 class App:
     def __init__(self, video_src):
         self.video_src = video_src
@@ -93,6 +165,9 @@ class App:
         prev_contours = []
         prev_contourMask = []
         prev_centers = []
+        
+        prev_motionLabels = []
+
         while True:
             ret, frame = self.cam.read()
             if not ret:
@@ -125,7 +200,6 @@ class App:
             closed = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
             closed = cv2.morphologyEx(closed, cv2.MORPH_OPEN, kernel)
 
-
             
             #########
             # Step 3: contour
@@ -137,8 +211,8 @@ class App:
             minLength = 2
             minArea = 20
             new_contours=[]
-            for c in contours:
-                if len(c) > minLength and cv2.contourArea(c) > minArea:
+            for index, c in enumerate(contours):
+                if len(c) > minLength and cv2.contourArea(c) > minArea: # and hierarchy[index] is not None:
                     new_contours.append(c)
             contours = new_contours
 
@@ -169,17 +243,24 @@ class App:
 
             # find relationship between contours and prev_contours
             new_contours = []
+            
+            motionLabels = [-1 for i in range(len(contours))]
             if len(prev_contours) > 0:
                 prev_flag = [0 for i in range(len(prev_contours))]
                 isSplit = [False for i in range(len(contours))]
                 relation = [0 for i in range(len(contours))] # index is contours, value is prev_contours
-                # find one split into two case
+                
+                if _DEBUG==True:
+                    import pdb
+                    pdb.set_trace()
+
+                #cv2.imshow("contour", prev_contourMask)
                 for i1, contour in enumerate(contours):
                     cx = centers[i1][0] 
                     cy = centers[i1][1]
                     if prev_contourMask[cy][cx][0] == 255:        
                         # find closest prev_contour and add 1
-                        minDistance = 0
+                        minDistance = 100000
                         minI2 = 0
                         for i2, prev_center in enumerate(prev_centers):
                             distance = abs(prev_center[0]-cx) + abs(prev_center[1]-cy)
@@ -190,7 +271,20 @@ class App:
                         if prev_flag[minI2] > 1:
                             isSplit[i1] = True
                         relation[i1] = minI2
-                
+                        motionLabels[i1] = prev_motionLabels[minI2]
+
+                # set motionLabels
+                for i, label in enumerate(motionLabels):
+                    if label == -1:
+                        cnt = 0
+                        while True:
+                            if cnt in motionLabels:
+                                cnt = cnt + 1
+                            else:
+                                motionLabels[i] = cnt
+                                break
+
+                # find one split into two case
                 #new_contours = []
                 haveRead = [False for i in range(len(prev_contours))]
                 for i1, contour in enumerate(contours):
@@ -201,23 +295,22 @@ class App:
                     else:
                         new_contours.append(contour)
                 #contours = new_contours
-            
-            print(len(contours))
-            print(len(new_contours))
+            else:
+                motionLabels = [i for i in range(len(contours))]
+           
+
             #cv2.imshow('mask', fgmask)
             #cv2.imshow("close", closed)
-            
-            hulls2 = []
-            for contour in new_contours:
-                hull = cv2.convexHull(contour)
-                hulls2.append(hull)
-            cv2.drawContours( contourMask, hulls2, -1, (255,0,255), 2)
-            
             #cv2.imshow("contour", contourMask)
 
             #cv2.drawContours( vis, contours, maxLength_index, (128,0,255), 3)
-            cv2.drawContours( vis, contours, -1, (255,255,255), 1)
-            cv2.drawContours( vis, new_contours, -1, (255,0,255), 1)
+            #cv2.drawContours( vis, contours, -1, (255,255,255), 1)
+            #cv2.drawContours( vis, new_contours, -1, (255,0,255), 1)
+            
+            for index, contour in enumerate(contours):
+                cv2.drawContours( vis, [contour], 0, color[motionLabels[index]%len(color)], 1)
+                draw_str(vis, centers[index], '%d' % motionLabels[index])
+
             cv2.imshow('lk_track', vis)
 
 
@@ -227,7 +320,7 @@ class App:
             prev_contours = len(new_contours) == 0 and contours or new_contours 
             prev_contourMask = contourMask
             prev_centers = centers
-
+            prev_motionLabels = motionLabels
 
             ch = 0xFF & cv2.waitKey(delayTime) # 20
             if ch == 27:
